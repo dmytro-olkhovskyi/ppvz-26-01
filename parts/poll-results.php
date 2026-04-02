baf95769ed62a85c531e73b9e82937771d3381d3
Dmytro Olkhovskyi <dmytro.olkhovskyi@puet.edu.ua>
2026-04-02 09:15:04 +0300

    Poll results

--- parts/poll-results.php
+++ parts/poll-results.php
@@ -8,6 +8,8 @@
         }*/
 
         $correct_count = 0;
+        $total = count($questions);
+
         foreach ($answers as $key => $value) {
             $question = $questions[$key];
             $answer = $question['answers'][$value];
@@ -23,10 +25,21 @@
 
             echo '<hr>';
         }
+        
+        echo '<hr>Correct ' . $correct_count . ' from ' . $total;
 
-        echo '<hr>Correct ' . $correct_count . ' from ' . count($questions);
+        $q_res = 'INSERT INTO results (correct, total) VALUES (?, ?)';
+        $q_st = $db->prepare($q_res);
+        $q_st->bind_param('ii', $correct_count, $total);
+        $q_st->execute();
+
+        // $res_q = $db->execute_query($q_res, [$correct_count, $total]);
 
     } else {
         echo '<p>Empty data!</p>';
     }
+
+
+    // Print all results (table)
+    //
 ?>
